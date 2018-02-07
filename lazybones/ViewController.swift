dabe4760b2995e361a4f78df5ca792988526938b
Brian Slack <brian.slack@allclearinsurance.com>
2018-02-07 16:42:04 +0000

    Initial Commit

---
+++ lazybones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lazybones
+//
+//  Created by Brian Slack on 07/02/2018.
+//  Copyright © 2018 Depicus Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
