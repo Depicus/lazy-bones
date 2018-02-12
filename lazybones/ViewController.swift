ca8c5db36bb65e6c50a53b3c553aa1a426e2a692
Brian Slack <brian.slack@allclearinsurance.com>
2018-02-12 23:06:01 +0000

    added ssdp discovery

--- lazybones/ViewController.swift
+++ lazybones/ViewController.swift
@@ -8,31 +8,48 @@
 
 import UIKit
 import wakeonlan
-//import Commands
+//import UDPBroadcast
 
 class ViewController: UIViewController {
     
     let command = Commands()
-
+    var broadcastConnection: UDPBroadcastConnection!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        broadcastConnection = UDPBroadcastConnection(port: 1900) { [unowned self] (ipAddress: String, port: Int, response: String?) -> Void in
+            print("Received from \(ipAddress):\(port):\n\n\(String(describing: response))")
+            //self.logView.text = log
+        }
+        
+        let buf = "M-SEARCH * HTTP/1.1\r\n" +
+            "MAN: \"ssdp:discover\"\r\n" +
+            "HOST: 239.255.255.250:1900\r\n" +
+            "ST: urn:schemas-sony-com:service:X_Telepathy:1\r\n" +
+        "MX: 3\r\n\r\n"
+        broadcastConnection.sendBroadcast(buf)
+        //createSocket()
+        
+
         let wol = wakeup()
         if (wol.wakeonlan(mac: "AC:9B:0A:F6:7A:1D")) {
             NSLog("hello we are true")
         }
+        //sendSSDP()
+        //broadcastConnection.sendBroadcast("Mary had a little lamb")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func btnPower(_ sender: UIButton) {
         sendCommand(command: "AAAAAQAAAAEAAAAVAw==")
     }
     
-
+    
     @IBAction func btnMute(_ sender: UIButton) {
         sendCommand(command: "AAAAAQAAAAEAAAAUAw==")
     }
@@ -91,5 +108,38 @@ class ViewController: UIViewController {
         task.resume()
     }
     
+    func sendSSDP() {
+        print("ssdp pressed");
+        
+        let buf = "M-SEARCH * HTTP/1.1\r\n" +
+            "MAN: \"ssdp:discover\"\r\n" +
+            "HOST: 239.255.255.250:1900\r\n" +
+            "ST: ssdp:all\r\n" +
+        "MX: 5\r\n\r\n"
+        let INADDR_ANY = in_addr(s_addr: 0)
+        let fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        var addr_in = sockaddr_in(sin_len: __uint8_t(MemoryLayout<sockaddr_in>.size), sin_family: sa_family_t(AF_INET), sin_port: htons(value: 1900), sin_addr: INADDR_ANY, sin_zero: (0,0,0,0, 0,0,0,0))
+        inet_aton("239.255.255.250", &addr_in.sin_addr);
+        
+        buf.withCString { cstr -> Void in
+            _ = withUnsafePointer(to: &addr_in) {
+                
+                let broadcastMessageLength = buf.lengthOfBytes(using: .isoLatin1)
+                print("buffer size is set to \(broadcastMessageLength)")
+                let p = UnsafeRawPointer($0).bindMemory(to: sockaddr.self, capacity: 1)
+                var udpflag: UInt32 = 1;
+                if setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &udpflag, socklen_t(MemoryLayout.size(ofValue: udpflag))) == -1
+                {
+                    print("Failed to set socket \(strerror(errno))")
+                }
+                // Send the message cstr
+                sendto(fd, buf.cString(using: .isoLatin1), broadcastMessageLength, 0, p, socklen_t(addr_in.sin_len))
+            }
+        }
+    }
+    
+    private func htons(value: CUnsignedShort) -> CUnsignedShort {
+        return (value << 8) + (value >> 8)
+    }
+    
 }
-
---
+++ ssdp/ssdp/ssdp.h
@@ -0,0 +1,19 @@
+//
+//  ssdp.h
+//  ssdp
+//
+//  Created by Brian Slack on 12/02/2018.
+//  Copyright © 2018 Depicus Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ssdp.
+FOUNDATION_EXPORT double ssdpVersionNumber;
+
+//! Project version string for ssdp.
+FOUNDATION_EXPORT const unsigned char ssdpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ssdp/PublicHeader.h>
+
+
